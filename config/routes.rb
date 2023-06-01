27ed183ea64e9db05a810b454a3c67358515dd43
Laurel Galecki <laurel.galecki@kellogg.northwestern.edu>
2023-06-01 13:43:20 +0000

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # get "/", :controller => "tacos", :action => "index"
+
+  get "/", :controller => "posts", :action => "index"
+  get "/login", :controller => "sessions", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
+
+  resources "posts"
+  resources "sessions"
+  resources "users"
 end
